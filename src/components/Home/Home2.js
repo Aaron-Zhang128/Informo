06bc7270af1517822f6dd9fa103761c58859c52e
Aaron-Zhang128 <145629034+Aaron-Zhang128@users.noreply.github.com>
2024-03-05 21:20:18 -0700

    Update Home2.js

--- src/components/Home/Home2.js
+++ src/components/Home/Home2.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
-import myImg from "../../Assets/bot.png";
+import myImg from "../../Assets/avatar.svg";
 import Tilt from "react-parallax-tilt";
 import {
   AiFillGithub,
@@ -40,7 +40,7 @@ function Home2() {
                     3
                 </b>
               </i>
-               criteria: 
+              criteria: 
               <i>
                 <br />
                 <b className="purple">1. Past Accuracy </b>
@@ -53,7 +53,7 @@ function Home2() {
           </Col>
           <Col md={4} className="myAvtar">
             <Tilt>
-              <img src={myImg} className="img-fluid" alt="avatar" style = "border-radius:1000px;"/>
+              <img src={myImg} className="img-fluid" alt="avatar"/>
             </Tilt>
           </Col>
         </Row>
